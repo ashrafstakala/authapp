84002257b1606bfea6c458ab6b6db88e4e27a93f
ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2019-01-02 15:48:02 +0200

    email refactor

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,7 +3,7 @@ import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
-	state = { text: '' };
+	state = { email: '' };
 
 
 	render () {
@@ -11,9 +11,10 @@ class LoginForm extends Component {
 			<Card>
 				<CardSection>
 					<Input
+					placeholder="user@gmail.com"
 					label="Email"
-					value={this.state.text}
-					onChangeText={text => this.setState({ text })}
+					value={this.state.email}
+					onChangeText={email => this.setState({ email })}
 					/>
 				</CardSection>
 
