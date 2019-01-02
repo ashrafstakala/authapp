87d5061bd0929eb0b946cb15fd3798065185c465
ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2019-01-02 12:24:28 +0200

    added text input for login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 import { Header } from './components/common';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp( {
+			apiKey: 'AIzaSyCHRjxysrfsYouV1UPJU7LZEvjutZoKb4A',
+    		authDomain: 'auth-app-d1cfa.firebaseapp.com',
+    		databaseURL: 'https://auth-app-d1cfa.firebaseio.com',
+    		projectId: 'auth-app-d1cfa',
+    		storageBucket: 'auth-app-d1cfa.appspot.com',
+    		messagingSenderId: '156293871363'
+		});
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An App</Text>
+				<LoginForm />
 			</View>
-			);
+		);
 	}
 }
 
